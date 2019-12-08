cb89056bb304a76c97d86a22364e0caa860bdece
Andrey Ibashev <ibashev1@gmail.com>
2019-12-08 17:47:35 +0300

    redone script for vote counting

--- counter_of_vote.py
+++ counter_of_vote.py
@@ -3,13 +3,16 @@
 import json
 import sys
 
-SIZE_OF_BLOCK = 1610
-DATA_STRIDE = 8+64
+SIZE_OF_BLOCK = 8 + 1024 + 65 * 2
+DATA_STRIDE = 8
 DATA_SIZE = 1024
 
 podschet={}
 
 with open(sys.argv[1], "rb") as f:
-	byte = f.read(SIZE_OF_BLOCK)
-        for i in range(DATA_STRIDE, DATA_SIZE):
-        
+    byte = f.read(SIZE_OF_BLOCK)
+    end = byte[DATA_STRIDE:].find(b'\x00')
+    data = json.dumps(byte[DATA_STRIDE:end]
+    print(data)
+    podschet[data['message']] += 1
+print(podschet)
--- simulation.py
+++ simulation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import socket
 import time
+import json
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 8888
@@ -10,11 +11,10 @@ HASH_SIZE = 64
 lst = [1, 0] * (BUFFER_SIZE)
 msg_arr = bytearray(lst)
 
-#msg_arr = "0000aabbccddee";
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 while True:
     s.send(msg_arr)
     data = s.recv(HASH_SIZE)
-    time.sleep(10.0)
+    time.sleep(5.0)
 s.close()
